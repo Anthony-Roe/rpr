7f2ac953919e3220b01d030c55b8f7336375c506
Anthony Roe <antroe36@gmail.com>
2023-12-28 13:02:28 -0500

    implementing working stats

--- channels/updateMembersList.js
+++ channels/updateMembersList.js
@@ -1,10 +1,91 @@
 const {  } = require("discord.js");
 const cron = require("node-cron");
-const { guildId } = require('../config.json');
+const { channels, guildId, roles, chars } = require('../config.json');
+const client = require('../index.js');
 
 module.exports = {
-    update() {
-        cron.schedule("* * * * *", function() {
-        })
+    start() {
+        UpdateChannel();
     },
+}
+
+function UpdateChannel()
+{
+    cron.schedule("* * * * *", () => {
+        console.log("Updating Stats")
+        client.guilds.fetch(guildId).then(
+            guild => {
+                // Cadet
+                guild.roles.fetch(roles.cadet).then(
+                    role => {
+                        guild.channels.fetch(channels.cadet).then(
+                            cadet => {
+                                cadet.setName(`Cᴀᴅᴇᴛs${chars.colon}${chars.space}${role.members.size}`)
+                            }
+                        )
+                    }
+                )
+                // Recruit
+                guild.roles.fetch(roles.recruit).then(
+                    role => {
+                        guild.channels.fetch(channels.recruit).then(
+                            cadet => {
+                                cadet.setName(`Rᴇᴄʀᴜɪᴛs${chars.colon}${chars.space}${role.members.size}`)
+                            }
+                        )
+                    }
+                )
+                // EchoWest
+                guild.roles.fetch(roles.echowest).then(
+                    role => {
+                        guild.channels.fetch(channels.echowest).then(
+                            cadet => {
+                                cadet.setName(`EᴄʜᴏWᴇsᴛ${chars.colon}${chars.space}${role.members.size}`)
+                            }
+                        )
+                    }
+                )
+                // EchoEast
+                guild.roles.fetch(roles.echoeast).then(
+                    role => {
+                        guild.channels.fetch(channels.echoeast).then(
+                            cadet => {
+                                cadet.setName(`EᴄʜᴏEᴀsᴛ${chars.colon}${chars.space}${role.members.size}`)
+                            }
+                        )
+                    }
+                )
+                // foxtrotwest
+                guild.roles.fetch(roles.foxtrotwest).then(
+                    role => {
+                        guild.channels.fetch(channels.foxtrotwest).then(
+                            cadet => {
+                                cadet.setName(`FᴏxᴛʀᴏᴛWᴇsᴛ${chars.colon}${chars.space}${role.members.size}`)
+                            }
+                        )
+                    }
+                )
+                // foxtroteast
+                guild.roles.fetch(roles.foxtroteast).then(
+                    role => {
+                        guild.channels.fetch(channels.foxtroteast).then(
+                            cadet => {
+                                cadet.setName(`FᴏxᴛʀᴏᴛEᴀsᴛ${chars.colon}${chars.space}${role.members.size}`)
+                            }
+                        )
+                    }
+                )
+                // reaper
+                guild.roles.fetch(roles.reaper).then(
+                    role => {
+                        guild.channels.fetch(channels.reaper).then(
+                            cadet => {
+                                cadet.setName(`𝗥𝗲𝗮𝗽𝗲𝗿𝘀${chars.colon}${chars.space}${role.members.size}`)
+                            }
+                        )
+                    }
+                )
+            }
+        );
+    });
 }
--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
 const { Client, Events } = require('discord.js');
 const fs = require('node:fs');
 const { guildId } = require('../config.json');
+const client = require('../index.js')
+const path = require('node:path');
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
@@ -10,13 +12,14 @@ module.exports = {
 	once: true,
 	execute() {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		const channelsPath = path.join("A:/Development/rpr/", 'channels');
+
+		const channelsPath = path.join(client.projectDir, 'channels');
 		const channelsFiles = fs.readdirSync(channelsPath).filter(file => file.endsWith('.js'));
 		for (const file of channelsFiles) {
 			const filePath = path.join(channelsPath, file);
 			const run = require(filePath);
-			if (run.update) {
-				run.update();
+			if (run.start) {
+				run.start();
 			}
 		}
 	},
